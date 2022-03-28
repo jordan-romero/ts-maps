d6bfc2a014696b812d73c6118d5eec7f0e52491e
jordles113 <jordan.t.romero@gmail.com>
2022-03-28 18:05:34 -0500

    adds google maps and type definition files for google maps

--- src/Company.ts
+++ src/Company.ts
@@ -12,7 +12,8 @@ export class Company {
     this.companyName = faker.company.companyName()
     this.catchPhrase = faker.company.catchPhrase()
     this.location = {
-      
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
     }
   }
 }
